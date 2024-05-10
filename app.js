db88ba4290dbc034716989af51fe4ce306cad16c
Mari0mori <ardutah9@gmail.com>
2024-05-10 01:43:50 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$('.navItem').css('color', 'white');
+
+$('#nav').css('background-color', '#145');
+
+$('ul li:nth-child(even)').css('background-color', '#ccc');
+
+$('ul li:nth-child(odd)').css('background-color', '#aaa');
+
+$('input').val('AceDavis');
